11a8fd56f86025e3c7cebd4a66a095e2308be165
francois-niang1 <francois.niang@laplateforme.io>
2023-03-27 12:38:09 +0200

    add com

--- jour01/job11.py
+++ jour01/job11.py
@@ -1,3 +1,8 @@
+# Demander le nom de l'utilisateur 
 print ("Comment tu t'appelles ? ")
+
+# recuperer son nom dans l'input et l'initialisé dans une variable
 name = input()
+
+# Afficher le resulat
 print ("Bonjour,", name)
