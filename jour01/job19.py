4e8ecb4015d8c361e8c159507d751ed9837c54ae
francois-niang1 <francois.niang@laplateforme.io>
2023-03-27 20:50:52 +0200

    19 done

--- jour01/job19.py
+++ jour01/job19.py
@@ -1,10 +1,13 @@
 def draw_rectangle(width, height):
 
-    # Afficher les lignes intermédiaires avec des '|' à gauche et à droite
+    # Afficher les lignes 
     for i in range(height):
+
+# afficher les premier et derniere ligne 
         if i == 0 or i == height-1:
             print("|" + "-" * (width - 2) + "|")
 
+# afficher les lignes intermediaires
         else:
             print("|" + " " * (width - 2) + "|")
     
